6bb8d1e728d91ba2455e1b61538a7e4310ba2e8f
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-09-18 17:03:58 -0400

    add unicode_tokenizer

---
+++ lib/boilerpipe/util/unicode_tokenizer.ex
@@ -0,0 +1,16 @@
+defmodule Boilerpipe.Util.UnicodeTokenizer do
+  def tokenize(text) do
+    # replace word boundaries with 'invisible separator'
+    # strip invisible separators from non-word boundaries
+    # replace spaces or invisible separators with a single space
+    # trim
+    # split words on spaces
+
+    text
+    |> String.replace(~r/\b/, "\u2063")
+    |> String.replace(~r/[\x{2063}]*(["'.,!@-\\:;$?()\/])[\x{2063}]*/u, "\\1")
+    |> String.replace(~r/[ \x{2063}]+/u, " ")
+    |> String.trim
+    |> String.split(~r/[ ]+/)
+  end
+end
