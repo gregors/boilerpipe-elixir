d626cdca3a41a386947ba4f84cfdf02fdf613bf7
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2019-01-01 13:33:36 -0500

    add Labels.Default

---
+++ lib/boilerpipe/labels/default.ex
@@ -0,0 +1,16 @@
+defmodule Boilerpipe.Labels.Default do
+  :TITLE
+  :ARTICLE_METADATA
+  :INDICATES_END_OF_TEXT
+  :MIGHT_BE_CONTENT
+  :VERY_LIKELY_CONTENT
+  :STRICTLY_NOT_CONTENT
+  :HR
+  :LI
+  :HEADING
+  :H1
+  :H2
+  :H3
+  @markup_prefix "<" 
+  def markup_prefix, do: @markup_prefix
+end
---
+++ test/labels/default_test.exs
@@ -0,0 +1,8 @@
+defmodule LabelsDefaultTest do
+  use ExUnit.Case
+  alias Boilerpipe.Labels.Default
+
+  test "has markup prefix" do
+    assert Default.markup_prefix == "<" 
+  end
+end
