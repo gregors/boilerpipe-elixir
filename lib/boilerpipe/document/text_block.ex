b6012600b0c6c434f1fc83c8cb53a83afa8d2ecd
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2020-07-23 22:55:27 -0400

    text document - add .new and .debug_s

--- lib/boilerpipe/document/text_block.ex
+++ lib/boilerpipe/document/text_block.ex
@@ -2,16 +2,20 @@ defmodule Boilerpipe.Document.TextBlock do
   defstruct num_words: 0,
             num_words_in_wrapped_lines: 0,
             num_words_in_anchor_text: 0,
-            num_wrapped_lines: 0,
+            num_wrapped_lines: 1,
             offset_blocks_start: 0,
             offset_blocks_end: 0,
             text: "",
             text_density: 0,
-            link_density: 0,
+            link_density: 0.0,
             labels: %MapSet{},
             tag_level: 0,
             num_full_text_words: 0,
             content: false
+
+  def new(text) do
+    %Boilerpipe.Document.TextBlock{text: text}
+  end
 
   def set_tag_level(text_block, level) do
     %{text_block | tag_level: level}
@@ -31,16 +35,17 @@ defmodule Boilerpipe.Document.TextBlock do
 
   def to_s(text_block) do
     labels =
-      if Enum.empty?(text_block.labels),
-        do: text_block.labels |> MapSet.to_list() |> Enum.join(","),
-        else: "null"
+      case Enum.empty?(text_block.labels) do
+        true -> "null"
+        _ -> text_block.labels |> MapSet.to_list() |> Enum.join(",")
+      end
 
     "[#{text_block.offset_blocks_start}-#{text_block.offset_blocks_end};\
-    tl=#{text_block.tag_level}; \
-    nw=#{text_block.num_words};\
-    nwl=#{text_block.num_wrapped_lines};\
-    ld=#{text_block.link_density}]\t#{
-      if text_block.is_content?, do: "CONTENT", else: "BOILERPLATE"
+tl=#{text_block.tag_level}; \
+nw=#{text_block.num_words};\
+nwl=#{text_block.num_wrapped_lines};\
+ld=#{text_block.link_density}]\t#{
+      if text_block.content, do: "CONTENT", else: "BOILERPLATE"
     },#{labels}\n#{text_block.text}"
   end
 end
---
+++ test/document/text_document_test.exs
@@ -0,0 +1,28 @@
+defmodule DocumentTextDocumentTest do
+  use ExUnit.Case
+  alias Boilerpipe.Document.TextDocument
+  alias Boilerpipe.Document.TextBlock
+
+
+  test ".new needs title and text_blocks" do
+    tbs = [ TextBlock.new("one"), TextBlock.new("two") ]
+    td = TextDocument.new("my title", tbs)
+    assert td == %{title: "my title", text_blocks: tbs}
+  end
+
+  test ".content returns the content" do
+    tbs =
+      [ TextBlock.new("one"), TextBlock.new("two") ]
+      |> Enum.map(fn tb -> %TextBlock{ tb | content: true } end)
+
+    td = TextDocument.new("my title", tbs)
+    assert TextDocument.content(td) == "one\ntwo"
+  end
+
+  test ".debug_s returns detailed debug info" do
+    tbs = [ TextBlock.new("one"), TextBlock.new("two") ]
+    td = TextDocument.new("my title", tbs)
+
+    assert TextDocument.debug_s(td) == "[0-0;tl=0; nw=0;nwl=1;ld=0.0]\tBOILERPLATE,null\none\n[0-0;tl=0; nw=0;nwl=1;ld=0.0]\tBOILERPLATE,null\ntwo"
+  end
+end
