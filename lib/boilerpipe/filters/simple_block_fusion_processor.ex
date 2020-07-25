2c3d5d569cdcce301653daf5e97ad8ee48229d93
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2020-07-25 01:00:56 -0400

    add simple_block_fusion_processor initial

---
+++ lib/boilerpipe/filters/simple_block_fusion_processor.ex
@@ -0,0 +1,15 @@
+defmodule Boilerpipe.Filters.SimpleBlockFusionProcessor do
+  def process(%{text_blocks: text_blocks}) when length(text_blocks) > 2 do
+    new_text_blocks = merge(text_blocks, [])
+    TextDocument.replace_text_blocks(doc, new_text_blocks)
+  end
+
+  def merge([first | []], acc), do: [first | acc] |> Enum.reverse()
+
+  def merge([first, second | tail], acc) do
+    case first.text_density == second.text_density do
+      true -> merge([TextBlock.merge(first, second) | tail], acc)
+      _ -> merge([second | tail], [first | acc])
+    end
+  end
+end
