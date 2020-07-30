86306329a44d2f95ae5738d08456640c21cf8c90
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2020-07-29 21:23:26 -0400

    rough out handle_event :start_element

--- lib/boilerpipe/sax/html_content_handler.ex
+++ lib/boilerpipe/sax/html_content_handler.ex
@@ -34,9 +34,27 @@ defmodule Boilerpipe.SAX.HtmlContentHandler do
     {:ok, state}
   end
 
-  def handle_event(:start_element, {name, attributes}, state) do
-    IO.inspect("Start parsing element #{name} with attributes #{inspect(attributes)}")
-    {:ok, [{:start_element, name, attributes} | state]}
+  def handle_event(:start_element, {name, attrs}, state) do
+    IO.inspect("Start parsing element #{name} with attributes #{inspect(attrs)}")
+    new_state = %{state | label_stacks: [nil | state.label_stacks]}
+
+    # TODO: get atom via a mapping function
+    tag = name |> String.upcase()
+
+    new_state =
+      case state.tag_actions[tag] do
+        {:ok, tag_action} ->
+          tag_level = tag_level(tag_action, new_state)
+          flush = tag_action.start(name, attrs) || new_state.flush
+          %{new_state | tag_level: tag_level, flush: flush}
+
+        _ ->
+          %{new_state | tag_level: new_state.tag_level + 1, flush: true}
+      end
+
+    new_state = %{new_state | last_event: :START_TAG, last_start_tag: tag}
+
+    {:ok, new_state}
   end
 
   def handle_event(:end_element, name, state) do
@@ -47,5 +65,13 @@ defmodule Boilerpipe.SAX.HtmlContentHandler do
   def handle_event(:characters, chars, state) do
     IO.inspect("Receive characters #{chars}")
     {:ok, [{:chacters, chars} | state]}
+  end
+
+  def tag_level(tag_action, state) do
+    if tag_action.changes_tag_level do
+      state.tag_level + 1
+    else
+      state.tag_level
+    end
   end
 end
