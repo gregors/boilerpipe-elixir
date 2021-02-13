45cba891d66b1869d294fd842a556403f06b03d0
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2021-02-13 15:00:05 -0500

    preprocessing

--- lib/boilerpipe/sax/parser.ex
+++ lib/boilerpipe/sax/parser.ex
@@ -1,7 +1,8 @@
 defmodule Boilerpipe.SAX.Parser do
   def parse(html) do
-    # html preparsing to fix weird stuff
-    #
+    # preprocessing
+    html = String.replace(html, ~r/<script.+<\/script>/is, "")
+
     state = Boilerpipe.SAX.HtmlContentHandler.new()
     Saxy.parse_string(html, Boilerpipe.SAX.HtmlContentHandler, state)
   end
